3e8422ba87b2c9ed435262d02639d1b7852e8edc
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-02 13:19:23 +0200

    add icon library

---
+++ src/Components/UI/Icons/Icons.jsx
@@ -0,0 +1,16 @@
+export const EyeIcon = ({ className = "", width = 22, height = 22 }) => (
+    <svg
+        width={width}
+        height={height}
+        className={className}
+        viewBox="0 0 24 24"
+        fill="none"
+        strokeWidth={2}
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+        <circle cx={12} cy={12} r={3} />
+    </svg>
+);
