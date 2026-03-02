11950248c6a488faf559f0775e1c1c31997abefd
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-02 13:37:04 +0200

    add icon library

--- src/Pages/Auth/Login/Login.jsx
+++ src/Pages/Auth/Login/Login.jsx
@@ -4,6 +4,7 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useCookies } from "react-cookie";
 import "./loginStyle.css";
+import { EmailIcon, EyeIcon, EyeOffIcon } from "../../../Components/UI/Icons/Icons";
 
 export default function Login() {
 
@@ -111,7 +112,7 @@ export function LoginForm() {
             onBlur={formik.handleBlur}
           />
           <div className="input_icon">
-            <img src="/Icons/Auth/email.svg" alt="email" />
+            <EmailIcon/>
           </div>
         </div>
       </div>
@@ -135,11 +136,7 @@ export function LoginForm() {
             onBlur={formik.handleBlur}
           />
           <div className="input_icon" onClick={() => setShowPassword(!showPassword)} aria-label={showPassword ? "إخفاء كلمة المرور" : "عرض كلمة المرور"}>
-            {showPassword ?
-              <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" /><circle cx={12} cy={12} r={3} /></svg>
-              :
-              <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-eye-off-icon lucide-eye-off"><path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49" /><path d="M14.084 14.158a3 3 0 0 1-4.242-4.242" /><path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143" /><path d="m2 2 20 20" /></svg>
-            }
+            {showPassword ? <EyeIcon/> : <EyeOffIcon/>}
           </div>
         </div>
       </div>
